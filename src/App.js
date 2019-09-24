ad41f4c555ba6567feeb73c688dc0976c9acf0df
Erik Irgens <erik.t.irgens@gmail.com>
2019-09-24 11:31:45 -0700

    Add props.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 // This component has a class that is returning (rendering) the jsx data within the return() method. It looks like HTML, but it is actually JSX that is read by our workflow. The comment below describes what is actually being rednered in this instnace.
 
@@ -8,6 +9,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Hi. I'm a react app!</h1>
+        <p>This is really wokring!</p>
+        <Person name="Erik" age="27" >Hobbies: Coding, writing music</Person>
+        <Person name="Katie" age="30" />
+        <Person name="Devon" age="30" />
       </div>
     );
     // This is what the above code is actually returning.
