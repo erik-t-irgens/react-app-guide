28f06ddcbdd12f4d531032cba97c23f43632fc95
Erik Irgens <erik.t.irgens@gmail.com>
2019-09-29 12:38:56 -0700

    change the setup to use react hooks, and well as state slices. No longer need 'this' to work with parts of a class, because we don't use a class anymore.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'; //No longer importing Component, because we aren't extended it with our app function, as we were with classes.
 import './App.css';
 import Person from './Person/Person';
 
 // This component has a class that is returning (rendering) the jsx data within the return() method. It looks like HTML, but it is actually JSX that is read by our workflow. The comment below describes what is actually being rednered in this instnace.
 
-class App extends Component {
-  state = {
+// This update is the hooks method of creating a component.
+const app = props => {
+  // Now uses useState as a const.
+  // personsState represents the state itself (access to the object, instead of this.state from before), setPersonsState represents the function to set state.
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Erik", age: 27 },
       { name: "Katie", age: 30 },
       { name: "Devon", age: 30 }
-    ],
-    otherState: "something else"
-  };
+    ]
+    // We remove OtherState from this object and create a new useState function to separate state. This is call state slices.
+  });
 
-  handleSwitchName = () => {
+  const [otherState, setOtherState] = useState('some other default value for state');
+
+
+  const handleSwitchName = () => {
     // console.log('Was clicked');
     // DON'T DO THIS
     // this.state.persons[0].name = "Erik Thomas Irgens"
-    this.setState({
+    // instead of this.setState, we use setPersonsState to set state with the destuctured array const above.
+    setPersonsState({
       persons: [
         { name: "Erik Thomas Irgens", age: 27 },
         { name: "Katie", age: 30 },
         { name: "Devon", age: 30 }
       ]
+      // Notice that OtherState is no manually placed in here. This will actually make it so the updated state removes whatever you don't have in there. 
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1> Hi. I'm a react app!</h1>
-        <p>This is really wokring!</p>
-        <button onClick={this.handleSwitchName}>Switch name!</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: Coding, writing music</Person>
-      </div>
-    );
-    // This is what the above code is actually returning.
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi. I\'m a react app!'));
-  }
+  // No longer a render() method wrapping this return.
+  return (
+    <div className="App">
+      <h1> Hi. I'm a react app!</h1>
+      <p>This is really wokring!</p>
+      <button onClick={handleSwitchName}>Switch name!</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hobbies: Coding, writing music</Person>
+    </div>
+  );
+  // This is what the above code is actually returning.
+  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi. I\'m a react app!'));
+
 }
 
-export default App;
+export default app;
+
+
