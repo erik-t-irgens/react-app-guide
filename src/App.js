06182a15ce33efd3e96e9a787a2eb7133d9a9d98
Erik Irgens <erik.t.irgens@gmail.com>
2019-09-29 13:51:39 -0700

    Text input frields that update displays.

--- src/App.js
+++ src/App.js
@@ -19,19 +19,31 @@ const app = props => {
 
   const [otherState, setOtherState] = useState('some other default value for state');
 
-
-  const handleSwitchName = () => {
+  // Now passing newName into the handle
+  const handleSwitchName = (newName) => {
     // console.log('Was clicked');
     // DON'T DO THIS
     // this.state.persons[0].name = "Erik Thomas Irgens"
     // instead of this.setState, we use setPersonsState to set state with the destuctured array const above.
     setPersonsState({
       persons: [
-        { name: "Erik Thomas Irgens", age: 27 },
+        { name: "Erik", age: 27 },
         { name: "Katie", age: 30 },
         { name: "Devon", age: 30 }
       ]
       // Notice that OtherState is no manually placed in here. This will actually make it so the updated state removes whatever you don't have in there. 
+    });
+  };
+
+
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: "Erik", age: 27 },
+        // This will take the value of our text input on persons, which we pass this down through props.
+        { name: event.target.value, age: 30 },
+        { name: "Devon", age: 30 }
+      ]
     });
   };
 
@@ -41,7 +53,16 @@ const app = props => {
       <h1> Hi. I'm a react app!</h1>
       <p>This is really wokring!</p>
       <button onClick={handleSwitchName}>Switch name!</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hobbies: Coding, writing music</Person>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+        // Add te handleSwitchName to props to allow u to click the p tag itself to use a specific function!!!
+        click={handleSwitchName}>Hobbies: Coding, writing music</Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        // Add te handleSwitchName to props to allow u to click the p tag itself to use a specific function!!!
+        changed={nameChangedHandler}>Hobbies: Coding, writing music</Person>
     </div>
   );
   // This is what the above code is actually returning.
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Person = (props) => {
     return (
         <div>
-            <p>I'm a person, my name is {props.name} and I'm {props.age}.</p>
+            <p onClick={props.click}>I'm a person, my name is {props.name} and I'm {props.age}.</p>
             <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name} />
         </div>
 
     )
